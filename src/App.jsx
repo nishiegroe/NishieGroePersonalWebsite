b52c1dac52cc45e6369ca7dba94b412e9901976a
Nishie Groe <nishiegroe@gmail.com>
2025-03-18 17:30:43 -0500

    Installing MaterialUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import profilePic from '../'
-import './App.css'
+import { useState } from "react";
+import profilePic from "../";
+import "./App.css";
 
 function App() {
-
   return (
-      <div className='app'>
-        <img src={profilePic} className="logo" alt="Vite logo" />
-        <h1>Hi! I'm Nishie.</h1>
-      </div>
-  )
+    <div className="app">
+      <img src={profilePic} className="logo" alt="Vite logo" />
+      <h1>Hi! I'm Nishie.</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyComponent: React = ({ name, age }) => {
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      {age && <p>You are {age} years old.</p>}
+    </div>
+  );
+};
+
+export default MyComponent;
