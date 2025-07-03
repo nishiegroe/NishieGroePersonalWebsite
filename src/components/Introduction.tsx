0fddc621db9eed539fbc51d6a9d20af99e9ac9ad
Nishie Groe <nishiegroe@gmail.com>
2025-07-02 22:21:03 -0500

    Making introduction short and sweet

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -52,55 +52,13 @@ const Introduction = () => {
                 <br />
                 <Typography
                     variant="body1"
-                    sx={{ textAlign: 'left' }}
+                    sx={{ textAlign: 'center' }}
                     fontSize="20px"
                 >
-                    <Typography
-                        variant="body1"
-                        sx={{ display: 'inline', fontWeight: 'bold' }}
-                        fontSize="20px"
-                    >
-                        Software Engineer
-                    </Typography>{' '}
-                    with a
-                    <Typography
-                        variant="body1"
-                        sx={{ display: 'inline', fontWeight: 'bold' }}
-                        fontSize="20px"
-                    >
-                        {" bachelor's degree"}
-                    </Typography>{' '}
-                    from WIU in{' '}
-                    <Typography
-                        variant="body1"
-                        sx={{ display: 'inline', fontWeight: 'bold' }}
-                        fontSize="20px"
-                    >
-                        Managed Information Systems
-                    </Typography>{' '}
-                    and{' '}
-                    <Typography
-                        variant="body1"
-                        sx={{ display: 'inline', fontWeight: 'bold' }}
-                        fontSize="20px"
-                    >
-                        over 7 years of experience
-                    </Typography>{' '}
-                    at a leading Fortune 500 company, excelling in full-stack
-                    development, frontend development, and Agile methodologies.
-                    Demonstrates a visionary approach to system architecture and
-                    user experience, leveraging skills in{' '}
-                    <Typography
-                        variant="body1"
-                        sx={{ display: 'inline', fontWeight: 'bold' }}
-                        fontSize="20px"
-                    >
-                        ReactJS/AngularJS, Java, microservices, and AWS
-                    </Typography>
-                    . Proven ability to lead international teams and streamline
-                    software lifecycles. Passionate about empowering teams and
-                    enhancing software lifecycles to achieve strategic business
-                    objectives.
+                    Full-stack Software Engineer with 7+ years of experience in
+                    React, Java, and AWS. Passionate about building great user
+                    experiences and leading teams to deliver impactful
+                    solutions.
                 </Typography>
             </Container>
             <Container
