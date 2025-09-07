e0f4b3cff690057c786abfaef401ea82c998f32b
Nishie Groe <nishiegroe@gmail.com>
2025-09-07 18:18:09 -0500

    Add type declarations for common static asset imports in TypeScript

---
+++ src/vite-env.d.ts
@@ -0,0 +1,20 @@
+// Allow importing common static assets in TS/TSX
+declare module '*.webp' {
+    const src: string
+    export default src
+}
+
+declare module '*.jpg' {
+    const src: string
+    export default src
+}
+
+declare module '*.png' {
+    const src: string
+    export default src
+}
+
+declare module '*.pdf' {
+    const src: string
+    export default src
+}
